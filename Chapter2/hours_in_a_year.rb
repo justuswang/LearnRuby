f29c3634d122284c5b29f29f06fd6d3079256953
zhawan01 <justus.wang@arm.com>
2013-10-15 10:21:38 +0800

    Add my scripts for Chapter2

---
+++ Chapter2/hours_in_a_year.rb
@@ -0,0 +1,1 @@
+puts (24 * 365).to_s + " OR " + (24 * 366).to_s + " for leap year"
---
+++ Chapter2/minutes_in_a_decade.rb
@@ -0,0 +1,4 @@
+puts 'Two Leap Years:'
+puts 60 * 24 * 365 * 8 + 60 * 24 * 366 * 2
+puts 'Three Leap Years:'
+puts 60 * 24 * 365 * 7 + 60 * 24 * 366 * 3
